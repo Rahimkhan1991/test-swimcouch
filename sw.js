0c5d21faeb5fec0098afd60d5459b032ddb01a35
Rahimkhan1991 <zafranalfarizi234@gmail.com>
2025-08-25 17:49:14 +0700

    03

--- sw.js
+++ sw.js
@@ -1,26 +1,31 @@
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("coachtrack-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "home.html",
-        "style.css",
-        "script.js",
-        "manifest.json",
-        "icons/icon-192.png",
-        "icons/icon-512.png"
-        // Add more assets if needed
-      ]);
-    })
-  );
+e.waitUntil(
+caches.open("coachtrack-cache").then(cache => {
+return cache.addAll([
+"/",
+"index.html",
+"home.html",
+"add-student.html",
+"admin.html",
+"attendance_report.html",
+"attendance.html",
+"fee.html",
+"finance_report.html",
+"menu.html",
+"sms.html",
+"view-students.html",
+"style.css",
+"manifest.json",
+"icons/icon-192.png",
+"icons/icon-512.png"
+]);
+})
+);
 });
-
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+e.respondWith(
+caches.match(e.request).then(response => {
+return response || fetch(e.request);
+})
+);
 });
-
-
